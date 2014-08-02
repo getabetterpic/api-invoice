66d069a1cbe4119850fc9773da84b6c7dec4dfcf
Daniel Smith <dan@getabetterpic.com>
2014-08-02 14:58:54 -0400

    Membership specs passing

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,6 @@
 class Company < ActiveRecord::Base
   validates_uniqueness_of :internal_reference, case_sensitive: false
   validates_presence_of :name, :internal_reference
+  has_many :memberships
   has_many :users, through: :memberships
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :omniauthable
+  has_many :memberships
+  has_many :companies, through: :memberships
 end
