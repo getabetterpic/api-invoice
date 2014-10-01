3fc54d59f6d5082270f3e9e81b9e1efd969dc976
Daniel Smith <dan@getabetterpic.com>
2014-10-01 11:01:16 -0400

    Added payment_date to payments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140929222219) do
+ActiveRecord::Schema.define(version: 20141001150036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,6 +116,7 @@ ActiveRecord::Schema.define(version: 20140929222219) do
     t.text     "notes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "payment_date"
   end
 
   add_index "payments", ["customer_id"], name: "index_payments_on_customer_id", using: :btree
